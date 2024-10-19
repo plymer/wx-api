9406a270bc1f1d0da0c36d22c1c763b83e252d87
plymer <93949066+plymer@users.noreply.github.com>
2024-10-19 16:39:09 -0600

    it is now working

---
+++ README.md
@@ -0,0 +1,12 @@
+# WX-API
+
+served at api.prairiewx.ca
+
+# Endpoints
+
+`/metars`
+
+Params:
+
+- `siteID`: 4-letter ICAO identifier for the site
+- `numHrs`: integer number of hours of requested METARs
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript Express!");
 });
 
-app.get("/hello", (req: Request, res: Response) => {
-  res.send("Another endpoint!");
-  console.log("hitting /hello");
-});
-
-app.listen(() => {
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
