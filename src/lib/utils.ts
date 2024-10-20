4727e75cda8d1904ffffe4592f060fba0fd7f7ef
plymer <93949066+plymer@users.noreply.github.com>
2024-10-19 21:16:25 -0600

    leadZero and ft-metre conversion

---
+++ src/lib/utils.ts
@@ -0,0 +1,10 @@
+export const FEET_PER_METRE = 3.28084;
+
+export function leadZero(input: number): string {
+  const inputString: string = input.toString();
+  let output: string;
+  inputString.length < 2
+    ? (output = "0" + inputString)
+    : (output = inputString);
+  return output;
+}
